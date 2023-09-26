4243317785101e5b2d3c9a9d57e41f7c9313b800
om-ica <irsacahaya17@gmail.com>
2023-09-26 07:50:19 +0700

    26/09/23

--- biayaekspedisi.java
+++ biayaekspedisi.java
@@ -11,43 +11,44 @@ public class biayaekspedisi {
     float brtBarang, jrkPengiriman, ttlHarga, hargaPerKilogram=0, hrgJarak=0;
     String jnsBarang, jnsPengiriman, estimasi="hari";
 
-    System.out.println("------------ INPUT HARGA ------------");
+    System.out.println("-------------------------------------- INPUT HARGA ------------------------------------------------");
 
-    System.out.println("Masukkan jenis barang (Barang.kecil/Barang.besar/Elektronik.kecil/Elektronik.besar/Pakaian/Dokumen): ");
-    jnsBarang=input.next();
-    System.out.println("Masukkan berat barang: ");
+    System.out.print("Masukkan jenis barang (Barang Kecil/Barang besar/Elektronik kecil/Elektronik besar/Pakaian/Dokumen)\n\t\t\t\t\t\t: ");
+    jnsBarang=input.nextLine();
+    System.out.print("Masukkan berat barang\t\t\t\t: ");
     brtBarang=input.nextFloat();
-    System.out.println("Masukkan jenis pengiriman(Reguler/Express): ");
-    jnsPengiriman=input.next();
-    System.out.println("Masukkan jarak pengiriman: ");
+    System.out.print("Masukkan jenis pengiriman(Reguler/Express)\t: ");
+    input.nextLine();
+    jnsPengiriman=input.nextLine();
+    System.out.print("Masukkan jarak pengiriman\t\t\t: ");
     jrkPengiriman=input.nextFloat();
 
     
     //menentukan harga
 
     //harga per kilogram menurut jenis barang & layanan
-    if (jnsBarang.equals("Barang.kecil")) {
+    if (jnsBarang.equals("Barang kecil")) {
         if (jnsPengiriman.equals("Reguler")) {
             hargaPerKilogram=3000.0F;    
         } else if (jnsPengiriman.equals("Express")) {
             hargaPerKilogram=5000.0F; 
         }
         
-    } else if (jnsBarang.equals("Barang.besar")) {
+    } else if (jnsBarang.equals("Barang besar")) {
         if (jnsPengiriman.equals("Reguler")) {
             hargaPerKilogram=10000.0F;
         } else if (jnsPengiriman.equals("Express")) {
             hargaPerKilogram=15000.0F;     
         }
         
-    } else if (jnsBarang.equals("Elektronik.kecil")) {
+    } else if (jnsBarang.equals("Elektronik kecil")) {
         if (jnsPengiriman.equals("Reguler")) {
             hargaPerKilogram=8000.0F;
         } else if (jnsPengiriman.equals("Express")) {
             hargaPerKilogram=10000.0F;
         }
 
-    } else if (jnsBarang.equals("Elektronik.besar")) {
+    } else if (jnsBarang.equals("Elektronik besar")) {
         if (jnsPengiriman.equals("Reguler")) {
             hargaPerKilogram=10000.0F;
         } else if (jnsPengiriman.equals("Express")) {
@@ -77,6 +78,10 @@ public class biayaekspedisi {
         hrgJarak=15000.0F;
     } else if (jrkPengiriman < 500 ) {
         hrgJarak=20000.0F;
+    } else if (jrkPengiriman < 700) {
+        hrgJarak=25000.0F;  
+    } else if (jrkPengiriman < 1000) {
+        hrgJarak=35000.0F;
     }
 
     //menentukan total harga
@@ -90,8 +95,10 @@ public class biayaekspedisi {
     }
 
     // output hasil
-    System.out.println("\nHarga pengiriman Rp: " + ttlHarga);
-    System.out.println("Estimasi waktu pengiriman: " + estimasi);
+    System.out.println("\nHarga pengiriman\t\t\t\t: Rp " + ttlHarga);
+    System.out.println("Estimasi waktu pengiriman\t\t\t: " + estimasi);
+
+    System.out.println("----------------------------------------------------------------------------------------------------");
 
     input.close();
     }
