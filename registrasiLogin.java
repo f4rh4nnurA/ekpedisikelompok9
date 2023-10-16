26a02cad8b85308905f8ec567aed062c67488016
f4rh4nnurA <144897656+f4rh4nnurA@users.noreply.github.com>
2023-10-16 20:05:42 +0700

    Perulangan While pada fitur login dan registrasi user

--- registrasiLogin.java
+++ registrasiLogin.java
@@ -1,66 +1,63 @@
-//Fitur Registrasi dan Login;
-import java.util.HashMap;
+//Fitur Registrasi dan Login
+
 import java.util.Scanner;
 
 public class registrasiLogin {
-    static HashMap<String, String> users = new HashMap<String, String>();
     public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
-        while (choice != 3) {
-            System.out.println("Pilih Opsi: ");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Quit");
-            choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice) {
-                case 1:
-                    registerUser(scanner);
-                    break;
-                case 2:
-                    loginUser(scanner);
-                    break;
-                case 3:
-                    break;
-                default:
-                    System.out.println("Pilihan tidak ditemukan");
+        try(Scanner scan = new Scanner (System.in)) {
+            String email = " ", password1 = " ", password2 = " ";
+            boolean valid = false;
+            int attempts = 0, maxattempts = 3;
+            
+            // Registrasi
+        while (!valid) { 
+            System.out.println("\t\tRegistrasi\t\t");
+            System.out.print("email\t\t:");
+            email = scan.next();
+            System.out.print("\npassword1\t\t:");
+            password1 = scan.next();
+            System.out.print("\nPassword2\t\t:");
+            password2 = scan.next();
+            if(password1.equals(password2)){
+                System.out.println("\nRegistrasi anda berhasil, silakan login");
+                valid = true;
+            }else{
+                System.out.println("\nPassword anda tidak sama, silakan coba lagi");
             }
+
+
+            
         }
-        scanner.close();
-    }
-
-    static void registerUser(Scanner input) {
-        System.out.println("Masukkan email:");
-        String email = input.nextLine();
-        System.out.println("Masukkan passowrd anda:");
-        String password = input.nextLine();
-        if (users.containsKey(email)) {
-            System.out.println("email sudah terdaftar.");
-        } else {
-            users.put(email, password);
-            System.out.println("Registration successful.");
+            //login
+        System.out.println("\n\t\tLogin\n\t\t");
+        System.out.println("Pilihan akun login");
+        System.out.println("1. admin");
+        System.out.println("2. user");
+        System.out.print("\nPilih akun login dengan angka (1-2) : ");
+        String pilihan = scan.next();
+        
+        //jika pilih akun user "2"
+        if (pilihan.equals("2") ) {
+        while (attempts < maxattempts) {
+            System.out.print("\n\t\tLogin User\t\t");
+            System.out.print("\nEmail\t\t: ");
+            String emaillogin = "";
+            emaillogin = scan.next();
+            System.out.print("\nPassword\t\t: ");
+            password1 = scan.next();
+            if(password1.equals(password2) && emaillogin.equals(email)){
+                System.out.println("Anda berhasil login");
+                break;
+            }else {
+                attempts++;
+                System.out.print("Login gagal");
+            if(attempts == 3){
+            System.out.print(", silakan hubungi admin");
         }
     }
-
-    static void loginUser(Scanner input) {
-        System.out.println("Masukkan email:");
-        String email = input.nextLine();
-        System.out.println("Masukkan password anda:");
-        String password = input.nextLine();
-        if (!users.containsKey(email)) {
-            System.out.println("Email atau password tidak benar.");
-        } else {
-            if (users.get(email).equals(password)) {
-                System.out.println("Login berhasil.");
-            } else {
-                System.out.println("Email atau password tidak benar.");
-            }
-        }   
-        }
-
+}
     }
-
-
-
-    
+        
+} 
+}
+}
