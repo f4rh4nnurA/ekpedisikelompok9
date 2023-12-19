22f59a7a2eeb725027a0105aa933253f6faf976e
f4rh4nnurA <farhannuralifian1@gmail.com>
2023-12-19 20:11:31 +0700

    penambahan fitur setelah registrasi

--- SistemEkspedisi.java
+++ SistemEkspedisi.java
@@ -12,7 +12,7 @@ public class SistemEkspedisi  {
                                                 {false, false},
                                                 {false, false},
                                                 {false, false},
-                                                {false, false}};
+                                                {false, false, false}};
     private static long[][] no_HP           = new long [2][2];
     private static int attempts, itemCount  = 0;
     private static Scanner input            = new Scanner(System.in);
@@ -35,7 +35,7 @@ public static void main(String[] arg) {
                                 int pilih = input.nextInt();
                                 if (pilih == 1) {
                                     loginUser(input);
-                                    
+                                    fiturUser(input);
                                 } else if (pilih == 2){
                                     valid[1][0] = false;
                                     return;
@@ -47,15 +47,15 @@ public static void main(String[] arg) {
                                 }
                                 
                             } else if (choose == 2) {
-                                loginUser(input);
                                 tampilkanOpsi_SetelahRegistrasi(input);
+                                fiturUser(input);
                             } else if (choose == 3){
                                 valid[1][0] = false;
                                 opsi = memilihAkun(input);
                             } else if (choose == 4) {
                                 System.exit(0);
                             } 
-                        }
+                            }
                     case 2 :
                         loginAdmin(input);
                         menuInventori();
@@ -73,6 +73,20 @@ public static void main(String[] arg) {
             input.nextLine();
         }  
     } while(opsi < 1 || opsi > 3);
+}
+private static int tampilkanOpsi_SetelahRegistrasi(Scanner input) {
+    System.out.print("\n\t\t\tPilihan setelah registrasi" + "\n1. Login" + "\n2. Kembali ke halaman awal" + "\n3. Keluar dari aplikasi" +"\nApakah anda ingin login, kembali ke halaman awal, atau keluar ? (1/2/3)\n");
+    int masukan = input.nextInt();
+    if (masukan == 1) {
+        if (valid[4][1] == true && valid[4][0] == true && valid[3][0] == true && valid[2][0] == true){
+            loginUser(input);
+        } else {
+            System.out.println("Anda belum registrasi,silakan registrasi dulu");
+            valid[1][0] = false;
+        }
+    }
+    return masukan;
+
 }
 private static int memilihAkun(Scanner input) {
     System.out.print("\n\t\t\t\tSelamat Datang di Aplikasi Xpedisi" +"\nPilih Opsi: " + "\n1. User" + 
@@ -199,29 +213,181 @@ private static String loginAdmin(Scanner input){
     }
     return null;  
 }
+private static void inputPesanan(String[][] dataAwal) {
+    Scanner input = new Scanner(System.in);
+    String estimasi = "hari";
+
+    System.out.print("Jumlah paket yang ingin anda kirimkan: ");
+    int jmlPaket = input.nextInt();
+
+    int[] resi = new int[jmlPaket];
+    double hargaAkhir = 0;
+
+    System.out.println("-------------------------------------- INPUT PESANAN ------------------------------------------------");
+    
+    // boolean tambahPesanan = true;
+    int i = checkEmptyArrayElement(dataAwal);
+    
+    int availableDataSpace = dataAwal.length - checkEmptyArrayElement(dataAwal);
+    
+    if (availableDataSpace < jmlPaket) {
+        System.out.println("Barang terlalu banyak!");
+        input.close();
+        return;
+    }
+
+
+    while (i < jmlPaket) {
+        System.out.println("\n--------------------------------- Paket ke-" + (i+1) + " ---------------------------------");
+        
+        Scanner input2 = new Scanner(System.in);
+        
+        System.out.print("Jenis barang: Barang Kecil/Barang besar/Elektronik kecil/Elektronik besar/Pakaian/Dokumen\n");
+        System.out.print("Masukkan jenis barang\t\t\t\t: ");
+        dataAwal[i+1][0] = input2.nextLine();
+        System.out.print("Berat barang\t\t\t\t\t: ");
+        dataAwal[i+1][1] = input2.nextLine();
+        System.out.print("Jenis pengiriman(Reguler/Express)\t\t: ");
+        dataAwal[i+1][2] = input2.nextLine();
+        System.out.print("Jarak pengiriman\t\t\t\t: ");
+        dataAwal[i+1][3] = input2.nextLine();
+        System.out.print("Asuransi(Iya/Tidak)\t\t\t\t: ");
+        dataAwal[i+1][4] = input2.nextLine();
+        System.out.print("Packing kayu(Iya/Tidak)\t\t\t\t: ");
+        dataAwal[i+1][5]=input2.nextLine();
+        
+        hitungBiaya(jmlPaket);
+        i++;
+        availableDataSpace = dataAwal.length - i;
+        
+    // ...
+    }
+}   
+
+private static void hitungBiaya(int jmlPaket) {
+    double hargaAkhir=0;
+    
+    int i = 0;
+    while (i < jmlPaket) {
+        if (dataAwal[i][0] != null) {
+        // & dataAwal[i][0] != null
+        double brtBarang = Double.parseDouble(dataAwal[i][1]);
+        double jrkPengiriman = Double.parseDouble(dataAwal[i][3]);
+        double ttlHarga, hargaPerKilogram = 0, hrgJarak = 0, hrgAsuransi = 0, hrgPackingKayu=0 ;
+        String estimasi = "";
+        //menentukan harga per kilogram
+        if (dataAwal[i][0].equalsIgnoreCase("Barang kecil")) {
+            if (dataAwal[i][2].equalsIgnoreCase("Reguler")) {
+                hargaPerKilogram = 3000.0;
+            } else if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+                hargaPerKilogram = 5000.0;
+            }
+        } else if (dataAwal[i][0].equalsIgnoreCase("Barang besar")) {
+            if (dataAwal[i][2].equalsIgnoreCase("Reguler")) {
+                hargaPerKilogram=10000.0;
+            } else if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+                hargaPerKilogram=15000.0;     
+            }
+            
+        } else if (dataAwal[i][0].equalsIgnoreCase("Elektronik kecil")) {
+            if (dataAwal[i][2].equalsIgnoreCase("Reguler")) {
+                hargaPerKilogram=8000.0;
+            } else if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+                hargaPerKilogram=10000.0;
+            }
+
+        } else if (dataAwal[i][0].equalsIgnoreCase("Elektronik besar")) {
+            if (dataAwal[i][2].equalsIgnoreCase("Reguler")) {
+                hargaPerKilogram=10000.0;
+            } else if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+                hargaPerKilogram=12000.0;
+            }
+            
+        } else if (dataAwal[i][0].equalsIgnoreCase("Pakaian")) {
+            if (dataAwal[i][2].equals("Reguler")) {
+                hargaPerKilogram=5000.0;
+            } else if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+                hargaPerKilogram=8000.0;
+            }
+        } else if (dataAwal[i][0].equalsIgnoreCase("Dokumen")) {
+            if (dataAwal[i][2].equalsIgnoreCase("Reguler")) {
+                hargaPerKilogram=2000.0;
+            } else if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+                hargaPerKilogram=4000.0;
+            }
+        }
+
+        //hitung harga
+        //harga per kilogram menurut jarak pengiriman
+        if (jrkPengiriman < 50) {
+            hrgJarak = 7000.0;
+        } else if (jrkPengiriman < 100) {
+            hrgJarak=10000.0;
+        } else if (jrkPengiriman < 300 ) {
+            hrgJarak=15000.0;
+        } else if (jrkPengiriman < 500 ) {
+            hrgJarak=20000.0;
+        } else if (jrkPengiriman < 700) {
+            hrgJarak=25000.0;  
+        } else if (jrkPengiriman < 1000) {
+            hrgJarak=35000.0;
+        }// ... (lanjutkan penentuan harga jarak lainnya)
+
+        //menentukan asuransi
+        if (dataAwal[i][4].equalsIgnoreCase("Iya")) {
+            hrgAsuransi = 10000.0;
+        } else if (dataAwal[i][4].equalsIgnoreCase("Tidak")) {
+            hrgAsuransi = 0.0;
+        }
+
+        if (dataAwal[i][5].equalsIgnoreCase("Iya")) {
+            hrgPackingKayu=25000.0;
+        } else if (dataAwal[i][5].equalsIgnoreCase("Tidak")) {
+            hrgPackingKayu=0.0;
+        }
+
+        if (dataAwal[i][2].equalsIgnoreCase("Reguler")) {
+            estimasi="7-10 Hari";
+        } if (dataAwal[i][2].equalsIgnoreCase("Express")) {
+            estimasi="3-5 Hari";
+        }
+
+        //hitung total harga
+        ttlHarga = (brtBarang * hargaPerKilogram) + hrgJarak + hrgAsuransi + hrgPackingKayu;
+        hargaAkhir += ttlHarga;
+
+        //output hasil
+        System.out.print("\nHarga pengiriman paket ke-"+ i + "\t\t\t\t: Rp " + ttlHarga);
+        System.out.print("\nEstimasi waktu pengirimanpaket ke-"+ i + "\t\t\t: " + estimasi + "\n");
+        // ... (lanjutkan output informasi lainnya)
+        }
+        // Tambahkan counter setiap kali melakukan loop
+        i++;
+    }
+        System.out.println("\nTotal Harga pengiriman untuk semua pesanan\t\t: Rp " + hargaAkhir);
+        System.out.println("\n----------------------------------------------------------------------------------------------------");
+}
+    
+
 public static void menuInventori() {
     boolean exitTrigger = false;
     while (!exitTrigger) {
         System.out.println("-------Menu Inventori------");
-        System.out.println("1. Menambahkan data paket");
-        System.out.println("2. Menunjukkan data paket");
-        System.out.println("3. Menghapus data paket");
-        System.out.println("4. Keluar");
+        System.out.println("1. Menunjukkan data paket");
+        System.out.println("2. Menghapus data paket");
+        System.out.println("3. Keluar");
         System.out.print("Masukkan opsi yang ingin anda pilih : ");
         int pilihan = input.nextInt();
         input.nextLine();
 
         switch (pilihan) {
             case 1:
-                tambahkanData();
-                break;
-            case 2:
                 tampilkanData();
                 break;
-            case 3:
+            case 2:
                 hapusData();
                 break;
-            case 4:
+            case 3:
                 exitTrigger = true;
                 break;
             default:
@@ -230,25 +396,13 @@ public static void menuInventori() {
     }
 }
 
-static void tambahkanData() {
-    if (itemCount < 10) {
-        System.out.println("Masukkan nama barang : ");
-        dataAwal[itemCount][0] = input.nextLine();
-        System.out.println("Masukkan alamat barang : ");
-        dataAwal[itemCount][1] = input.nextLine();
-        itemCount++;
-    } else {
-        System.out.println("Kapasitas data penuh");
-    }
-}
 
 static void tampilkanData() {
-    System.out.println("Nama barang , alamat ");
-    for (int i = 0; i < itemCount; i++) {
+    for (int i = 1; i < dataAwal.length; i++) {
         if (dataAwal[i][0] == null && dataAwal[i][1] == null) {
-            System.out.println((i + 1) + ". " + "Empty");
+            System.out.println((i) + ". " + "Empty");
         } else {
-            System.out.println((i + 1) + ". " + dataAwal[i][0] + " , " + dataAwal[i][1]);
+            System.out.println((i) + ". " + "Jenis barang: " + dataAwal[i][0] +", " + "Berat barang: " + dataAwal[i][1]+ ", " + "Tipe pengiriman: " + dataAwal[i][2] + ", " +  "Jarak Pengiriman: " + dataAwal[i][3]+ ", " +  "Asuransi: " + dataAwal[i][4]+ ", " +  "Packaging kayu:" + dataAwal[i][5]);
         }
     }
 }
@@ -261,9 +415,13 @@ static void hapusData() {
     String deleteConfirmation = input.next();
     input.nextLine();
     if (deleteConfirmation.equalsIgnoreCase("y")) {
-        if (deletedIndex >= 1 && deletedIndex <= itemCount) {
-            dataAwal[deletedIndex - 1][0] = null;
-            dataAwal[deletedIndex - 1][1] = null;
+        if (deletedIndex >= 1 && deletedIndex <= dataAwal.length) {
+            dataAwal[deletedIndex ][0] = null;
+            dataAwal[deletedIndex ][1] = null;
+            dataAwal[deletedIndex ][2] = null;
+            dataAwal[deletedIndex ][3] = null;
+            dataAwal[deletedIndex ][4] = null;
+            dataAwal[deletedIndex ][5] = null;
             System.out.println("Data berhasil dihapus");
         } else {
             System.out.println("Index tidak valid");
@@ -273,120 +431,42 @@ static void hapusData() {
     }
 }
 
-static double hitungBiaya(String jnsBarang, double brtBarang, String jnsPengiriman, double jrkPengiriman, String asuransi, String pckgKayu) {
-    double hargaPerKilogram = 0, hrgJarak = 0, hrgAsuransi = 0, hrgPackingKayu = 0, ttlHarga = 0;
-
-    // Menentukan harga per kilogram berdasarkan jenis barang & layanan pengiriman
-    if (jnsBarang.equalsIgnoreCase("Barang kecil")) {
-        if (jnsPengiriman.equalsIgnoreCase("Reguler")) {
-            hargaPerKilogram=3000.0;    
-        } else if (jnsPengiriman.equalsIgnoreCase("Express")) {
-                hargaPerKilogram=5000.0; 
-            }
-
-    } else if (jnsBarang.equalsIgnoreCase("Barang besar")) {
-            if (jnsPengiriman.equalsIgnoreCase("Reguler")) {
-                hargaPerKilogram=10000.0;
-            } else if (jnsPengiriman.equalsIgnoreCase("Express")) {
-                hargaPerKilogram=15000.0;     
-            }
-
-    } else if (jnsBarang.equalsIgnoreCase("Elektronik kecil")) {
-            if (jnsPengiriman.equalsIgnoreCase("Reguler")) {
-                hargaPerKilogram=8000.0;
-            } else if (jnsPengiriman.equalsIgnoreCase("Express")) {
-                hargaPerKilogram=10000.0;
-            }
-
-    } else if (jnsBarang.equalsIgnoreCase("Elektronik besar")) {
-            if (jnsPengiriman.equalsIgnoreCase("Reguler")) {
-                hargaPerKilogram=10000.0;
-            } else if (jnsPengiriman.equalsIgnoreCase("Express")) {
-                hargaPerKilogram=12000.0;
-            }
-
-    } else if (jnsBarang.equalsIgnoreCase("Pakaian")) {
-            if (jnsPengiriman.equals("Reguler")) {
-                hargaPerKilogram=5000.0;
-            } else if (jnsPengiriman.equalsIgnoreCase("Express")) {
-                hargaPerKilogram=8000.0;
-            }
-    } else if (jnsBarang.equalsIgnoreCase("Dokumen")) {
-            if (jnsPengiriman.equalsIgnoreCase("Reguler")) {
-                hargaPerKilogram=2000.0;
-            } else if (jnsPengiriman.equalsIgnoreCase("Express")) {
-                hargaPerKilogram=4000.0;
-            }
+static int checkEmptyArrayElement(String[][] dataAwal)
+{
+    for (int i = 0; i <= dataAwal.length; i++) {
+        if (dataAwal[i][0] == null) {
+            return i == 0 ? 0 : (i + 1);
+        }
     }
 
-    // Menentukan harga pengiriman berdasarkan jarak
-    if (jrkPengiriman < 50) {
-        hrgJarak=7000.0;
-    } else if (jrkPengiriman < 100) {
-        hrgJarak=10000.0;
-    } else if (jrkPengiriman < 300 ) {
-        hrgJarak=15000.0;
-    } else if (jrkPengiriman < 500 ) {
-        hrgJarak=20000.0;
-    } else if (jrkPengiriman < 700) {
-        hrgJarak=25000.0;  
-    } else if (jrkPengiriman < 1000) {
-        hrgJarak=35000.0;
+    return dataAwal.length;
+}
+private static void informasiAkun (Scanner input){
+    System.out.println("\n\t\t\tInformasi Akun" + "\nUsername" + usernameUser[0][0] +
+                        "\nEmail\t\t: " + emailUser[0][0] +
+                        "\nNomor Telepon\t\t:" + no_HP[0][0] +
+                        "\nPassword\t\t:" + passwordUser[0][0] );
+}
+private static String fiturUser (Scanner input){
+    System.out.println("\n\t\t\tDaftar Fitur User"  + 
+                    "\n1. Kirim Barang"             + 
+                    "\n2. Informasi Akun"           +
+                    "\nPilih Fitur yang ingin digunakan\t:");
+    int gunakan = input.nextInt();
+    if (gunakan == 1) {
+        inputPesanan(dataAwal);
+        hitungBiaya(gunakan);
+    } else if (gunakan == 2) {
+        informasiAkun(input);
     }
-    // ... Lanjutkan penentuan hargaJarak untuk jarak lainnya sesuai kebutuhan
+    return null;
 
-
-
-    // Menentukan harga asuransi
-    if (asuransi.equalsIgnoreCase("Iya")) {
-        hrgAsuransi=10000.0;
-    } else if (asuransi.equalsIgnoreCase("Tidak")) {
-        hrgAsuransi=0.0;
-    }
-
-    // Menentukan harga packing kayu
-    if (pckgKayu.equalsIgnoreCase("Iya")) {
-        hrgPackingKayu = 25000.0;
-    } else if (pckgKayu.equalsIgnoreCase("Tidak")) {
-        hrgPackingKayu = 0.0;
-    }
-
-    // Menghitung total harga
-    ttlHarga = (brtBarang * hargaPerKilogram) + hrgJarak + hrgAsuransi + hrgPackingKayu;
-
-    return ttlHarga;
+    
 }
 
-static void tampilkanPesanan(int[] paket, String[] jmlBarang, double hargaAkhir) {
-    System.out.println("\n----------------------------------------------------------------------------------------------------");
-    for (int j = 0; j < paket.length; j++) {
-        System.out.print("\nJenis barang ke-" + (j + 1) + " \t\t\t\t: " + jmlBarang[j]);
-    }
+}   
 
-    System.out.print("\nTotal Harga pengiriman\t\t\t\t: Rp " + hargaAkhir);
-    System.out.println("\n----------------------------------------------------------------------------------------------------");
 
-}
-
-static void tampilkanInfoAkun(Scanner input){
-    System.out.println("\n\t\t\tInformasi Akun" + 
-                        "\nUsername\t\t: "   + usernameUser[0][0] + 
-                        "\nEmail\t\t\t: "    + emailUser[0][0]    + 
-                        "\nNo.Telpon\t\t: "  + no_HP[0][0]        + 
-                        "\nPassword\t\t:"    + passwordUser[0][0]);
-}
-static int tampilkanOpsi_SetelahRegistrasi (Scanner input){
-    System.out.print("\nMenu Fitur " + "\n1. Kirim Barang" + "\n2. Akun" + "\nPilih opsi : ");
-    int pilihan = input.nextInt();
-    if (pilihan == 1) {
-                                        
-    } else if (pilihan == 2){
-        System.out.println("\n\t\t\tAkun");
-        tampilkanInfoAkun(input);
-    }
-    return input.nextInt();
-}
-}
 
 
 
